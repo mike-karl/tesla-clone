4b4bfa885517a64be5633a9504f4218b6b28c995
mike-karl <mike.karl1196@gmail.com>
2022-11-29 01:29:26 -0600

    Added a BtnLink component so the Sticky Btn Links could be customized and passed to the StickyContent Component

---
+++ src/Components/PageSection/BtnLink.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type Props = {
+    btnType: "primary" | "secondary",
+    title?: string
+    href: string,
+    children: string
+}
+
+const BtnLink = ({btnType, title, href, children}: Props) => {
+    if (title === null) {
+        title = children
+    }
+  return (
+    btnType === 'primary' ?
+    <a
+    title={title}
+    className={`primary-btn-link`}
+    href={href}
+>
+    {children}
+</a>
+: btnType === 'secondary' ?
+<a
+    title={title}
+    className={`secondary-btn-link`}
+    href={href}
+>
+    {children}
+</a>
+: null
+  )
+}
+
+export default BtnLink
