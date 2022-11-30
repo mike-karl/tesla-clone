a009e391d6cb3cb37c0bd95da7d8143ed6761b6e
mike-karl <mike.karl1196@gmail.com>
2022-11-29 19:58:18 -0600

    Updated the Footer component to be responsive. returns different footer depending on view port width

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,43 +2,112 @@ import React from 'react'
 
 type Props = {
     entry: IntersectionObserverEntry | undefined,
+    width: number,
+    breakpoints: {small: number, med: number},
+
 }
 
-const Footer = ({entry}: Props) => {
+const Footer = React.forwardRef<HTMLDivElement, Props>(({entry, width, breakpoints}, footerRef) => {
+    const { small, med } = breakpoints
   return (
-    <footer style={{opacity: `calc((2.5 * ${entry?.intersectionRatio}) - 1.5)`}}>
-      <nav>
-        <ul>
-          <li>
-            <a href="">
-              <span>Tesla</span>
-            </a>
-          </li>
-          <li>
-            <a href="">Privacy & Legal</a>
-          </li>
-          <li>
-            <a href="">Vehical Recalls</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-          <li>
-            <a href="">Careers</a>
-          </li>
-          <li>
-            <a href="">News</a>
-          </li>
-          <li>
-            <a href="">Engage</a>
-          </li>
-          <li>
-            <a href="">Location</a>
-          </li>
-        </ul>
-      </nav>
-    </footer>
+    <>
+      {width < small ? (
+        <footer ref={footerRef}>
+          <nav>
+            <ul>
+              <li>
+                <a href="">
+                  <span>Tesla</span>
+                </a>
+              </li>
+              <li>
+                <a href="">Privacy & Legal</a>
+              </li>
+              <li>
+                <a href="">Vehical Recalls</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">News</a>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+      ) : ( (width >= small) && (width < med) ) ? (
+        <footer
+          style={{ opacity: `calc((2.5 * ${entry?.intersectionRatio}) - 1.5)` }}
+        >
+          <nav>
+            <ul>
+              <li>
+                <a href="">
+                  <span>Tesla</span>
+                </a>
+              </li>
+              <li>
+                <a href="">Privacy & Legal</a>
+              </li>
+              <li>
+                <a href="">Vehical Recalls</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">News</a>
+              </li>
+              <li>
+                <a href="">Engage</a>
+              </li>
+              <li>
+                <a href="">Location</a>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+      ) : (
+        <footer
+        style={{ opacity: `calc((2.5 * ${entry?.intersectionRatio}) - 1.5)` }}
+      >
+        <nav>
+          <ul>
+            <li>
+              <a href="">
+                <span>Tesla</span>
+              </a>
+            </li>
+            <li>
+              <a href="">Privacy & Legal</a>
+            </li>
+            <li>
+              <a href="">Vehical Recalls</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a href="">Careers</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
+            <li>
+              <a href="">Engage</a>
+            </li>
+            <li>
+              <a href="">Location</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+      )}
+    </>
   );
-}
+})
 
 export default Footer
